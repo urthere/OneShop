6dc2797e7a439a6ff052d71182d472817bab7adc
urthere <zhyrain@msn.com>
2018-11-11 23:16:46 +0800

    add discount, subtotals for order details

---
+++ OneShop.Model/ViewModel/DiscountViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace OneShop.Model
+{
+    public class DiscountViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private IList<Discount> discounts;
+
+        public DiscountViewModel(string con)
+        {
+            using (var context = new OneShopEntities())
+            {
+                context.Database.Connection.ConnectionString = con;
+
+                try
+                {
+                    discounts = context.Discounts.ToList();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        public IList<Discount> Discounts => discounts;
+    }
+}
